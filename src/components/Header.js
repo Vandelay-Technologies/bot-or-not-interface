bda86cf85098abfed1dff75345eaeef1ca512846
Oliver <172459075+king1oliver@users.noreply.github.com>
2024-07-11 11:18:20 +0800

    replaced a with react components

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,20 +8,20 @@ const Header = ({ user, balance }) => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">BOT OR NOT</a>
+        <Link className="nav-link" to="/">BOT OR NOT</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <Link className="nav-link" to="/">Home</Link>
             </li>
             {!user ? (
               <li className="nav-item"><LoginButton /></li>
             ) : (
               <>
-                <li className="nav-item"><Link to="/profile">{user.username}</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/profile">{user.username}</Link></li>
                 <li className="nav-item"><CreditBalanceIcon balance={balance} /></li>
                 <li className="nav-item"><LogoutButton /></li>
               </>
